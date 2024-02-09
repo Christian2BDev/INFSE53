688ef0e04e31d5537f4e75274f9e5b8f3c07fad4
Christian2BDev <christiantenbrinke@hotmail.com>
2024-02-09 14:50:00 +0100

    ammo

--- COproj/Assets/Scenes/Game/scripts/shooting.cs
+++ COproj/Assets/Scenes/Game/scripts/shooting.cs
@@ -1,28 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 public class shooting : MonoBehaviour
 {
     public int maxdistance;
     public LayerMask hittable_items;
+    public int maxAmmo;
+    public int ammo;
+    public TMP_Text ammoTxt;
+    public GameObject outTxt;
 
-  
-
-     void Update()
+    void Start()
     {
+        StartCoroutine(Fade());
+        ammo = maxAmmo;
+        ammoTxt.text = ammo.ToString() + "/" + maxAmmo.ToString();
+    }
+    void Update()
+    {
+        
         if (Input.GetKeyDown(KeyCode.Space) || OVRInput.GetDown(OVRInput.RawButton.LIndexTrigger))
         {
-            GetComponent<health>().Damage();
-            if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, maxdistance, hittable_items))
-            {
-                Debug.Log(hit.collider.gameObject.name + " got hit");
-                Debug.DrawRay(transform.position, transform.forward, Color.green, 10000f); print("Hit");
-                
-            }
+            if (ammo > 0) shoot();
+            else StopAllCoroutines();
         }
     }
 
+    void shoot() {
+        //debug health
+        GetComponent<health>().Damage();
+        //----------
+        ammo--;
+        ammoTxt.text = ammo.ToString() + "/" + maxAmmo.ToString();
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, maxdistance, hittable_items))
+            {
+                Debug.Log(hit.collider.gameObject.name + " got hit");
+                Debug.DrawRay(transform.position, transform.forward, Color.green, 10000f); print("Hit");
+
+        }
+        
+    }
+
    
+
+    IEnumerator Fade()
+    {
+        Debug.Log("flik");
+        yield return new WaitForSeconds(10);
+        StopCoroutine(Fade());
+        StartCoroutine(Fade());
+
+    }
+
+
 }
 
